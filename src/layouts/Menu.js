38c7695796d4afaf17a8c6548dbc15ef62f76488
jiangyuzhen <1191973018@qq.com>
2018-02-06 16:30:14 +0800

    初始化项目

---
+++ src/layouts/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu">
+            菜单
+      </div>
+    );
+  }
+}
+
+export default Menu;
---
+++ src/pages/Index/Info/index.js
@@ -0,0 +1,17 @@
+/**
+ * @desc 社区介绍板块
+ * @author 
+ */
+import React, { Component } from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div className="Panel">
+                社区信息
+            </div>
+        );
+    }
+}
+
+export default Index;
