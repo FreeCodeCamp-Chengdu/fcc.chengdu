a855c2fed9b0cb3676559dee323474a93541a3da
CanbingChen <chencanbing@wolongdata.com>
2018-02-08 20:55:36 +0800

    add members

---
+++ src/pages/Index/Members/MembersConfig.js
@@ -0,0 +1,89 @@
+import ccbImg from '../../../assets/images/ccb-min.jpg';
+import akImg from '../../../assets/images/Ak-min.jpg';
+import glowd from '../../../assets/images/glowd-min.jpg';
+import Dean from '../../../assets/images/Dean-min.jpg';
+import Helen from '../../../assets/images/Helen-min.jpg';
+import janeZhen from '../../../assets/images/janeZhen-min.jpg';
+import lanhao from '../../../assets/images/lanhao-min.jpg';
+import liunanbing from '../../../assets/images/liunanbing-min.jpg';
+import qianyu from '../../../assets/images/qianyu-min.jpg';
+import qingsong from '../../../assets/images/qingsong-min.jpg';
+import shiyao from '../../../assets/images/shiyao-min.jpg';
+import Too from '../../../assets/images/Too-min.jpg';
+import wangbo from '../../../assets/images/wangbo-min.jpg';
+import wangjie from '../../../assets/images/wangjie-min.jpg';
+import xiaoshan from '../../../assets/images/xiaoshan-min.jpg';
+import yuqi from '../../../assets/images/yuqi-min.jpg';
+
+const MEMBERS = [{
+  name : '姜姜姜',
+  img : janeZhen,
+  responsibility:'社区负责人',
+  url: 'https://github.com/jiangyuzhen'
+},{
+  name : '袁民成',
+  img : Dean,
+  responsibility:'文案与周边',
+  url: 'https://github.com/jiangyuzhen'
+},{
+  name : 'Nanbing',
+  img : liunanbing,
+  responsibility:'协助运营',
+  url: 'http://nanbing.me/'
+},{
+  name : 'Helen',
+  img : Helen,
+  responsibility:'活动部',
+  url: 'https://github.com/TingYinHelen'
+},{
+  name : '初一',
+  img : wangbo,
+  responsibility:'设计',
+  url: 'http://918930.lofter.com'
+},{
+  name : '蓝浩',
+  img : lanhao,
+  responsibility:'后备开发人员',
+  url: 'http://www.lanhao.name'
+},{
+  name : 'glowd',
+  img : glowd,
+  responsibility:'后备开发人员',
+  url: 'https://github.com/Glowdable'
+},{
+  name : '书香墨剑',
+  img : yuqi,
+  responsibility:'后备开发人员',
+  url: ''
+},{
+  name : '蔡婷',
+  img : '',
+  responsibility:'活动策划',
+  url: 'https://tech-query.me/'
+},{
+  name : '水歌',
+  img : shiyao,
+  responsibility:'JavaScript 全栈开发讲师',
+  url: 'https://tech-query.me/'
+},{
+  name : '何青松',
+  img : qingsong,
+  responsibility:'FCC成都社区商务合作',
+  url: 'https://github.com/Heqingsong'
+},{
+  name : '陈小山',
+  img : xiaoshan,
+  responsibility:'活动策划执行',
+  url: 'https://github.com/HillChen3'
+},{
+  name : 'TOO',
+  img : Too,
+  responsibility:'技术支持',
+  url: 'http://fctoo.com'
+},{
+  name : '陈灿冰',
+  img : ccbImg,
+  responsibility:'技术支持',
+  url: 'https://github.com/CanbingChen'
+}];
+export default MEMBERS;
--- src/pages/Index/Members/index.js
+++ src/pages/Index/Members/index.js
@@ -1,18 +1,25 @@
 /**
  * @desc 社区组织者板块
- * @author 
+ * @author
  */
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import './style.css';
+import Card from './Card';
+import MEMBERS from './MembersConfig'
 class Index extends Component {
-    render() {
-        return (
-            <div className="Panel">
-                组织者风采展
-                <div style={{color: '#aaa'}}>备注：成员风采照、个人链接、昵称、社区中负责的职责</div>
-            </div>
-        );
-    }
+  renderMembers(){
+    return <div>
+      {MEMBERS.map(item =>{ return <Card detailData={item} key={item.name}/>})}
+    </div>
+  }
+	render() {
+		return (
+			<div className="Panel members">
+				组织者风采展
+				{this.renderMembers()}
+			</div>
+		);
+	}
 }
 
 export default Index;
