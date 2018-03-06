28761aa3779fd1bdeeeade20f1a53ebedddb7c37
loketa <yyuuqqii@outlook.com>
2018-03-06 15:12:44 +0800

    change form

--- src/layouts/About/data.js
+++ src/layouts/About/data.js
@@ -4,25 +4,21 @@ import qrWeibo from '../../assets/images/qr-weibo.png';
 import qrGithub from '../../assets/images/qr-github.png';
 
 const details = [{
-        "title": "weixin",
-        "desc": "官方微信公众号",
-        "img": qrWechat
-    },
-    {
-        "title": "qq",
-        "desc": "官方QQ群",
-        "img": qrQq
-    },
-    {
-        "title": "weibo",
-        "desc": "官方微博",
-        "img": qrWeibo
-    },
-    {
-        "title": "github",
-        "desc": "官方github",
-        "img": qrGithub
-    },
-];
+    "title": "weixin",
+    "desc": "官方微信公众号",
+    "img": qrWechat
+  },{
+    "title": "qq",
+    "desc": "官方QQ群",
+    "img": qrQq
+  },{
+    "title": "weibo",
+    "desc": "官方微博",
+    "img": qrWeibo
+  },{
+    "title": "github",
+    "desc": "官方github",
+    "img": qrGithub
+  }];
 
 export default details;
--- src/pages/Sponsors/index.js
+++ src/pages/Sponsors/index.js
@@ -7,21 +7,21 @@ import sponsors from './data';
 import './style.css';
 
 class Sponsors extends Component {
-    render() {
-        return (
-            <div className="row logolist">
-                {
-                    sponsors.map((ele,index) => {
-                        return (
-                            <div className="col-md-2 col-xs-4">
-                                <img src={ele.img} alt={ele.title} className="logo-img"/>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="row logolist">
+        {
+          sponsors.map((ele,index) => {
+            return (
+              <div className="col-md-2 col-xs-4">
+                  <img src={ele.img} alt={ele.title} className="logo-img"/>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default Sponsors;
