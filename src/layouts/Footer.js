8b39e95488c0da212c2b8c3897589d6886b4301f
captainblue2013 <admin@lanhao.name>
2018-03-30 00:44:09 +0800

    响应式

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
     return (
       <footer className="Footer">
         <div className="row justify-content-center align-items-center footer-bg">
-          <div className="col-xs-6">
+          <div className="col-xs-12 col-md-6">
             <div className="left-content">
               <img src={FLogo} alt="FCC logo"></img>
               <p>
@@ -15,7 +15,7 @@ class Footer extends Component {
               </p>
             </div>
           </div>
-          <div className="col-xs-6">
+          <div className="col-xs-12 col-md-6">
             <div className="right-content">
               <About />
             </div>
@@ -23,10 +23,10 @@ class Footer extends Component {
         </div>
         <div className="info justify-content-center align-items-center">
           <div className="row">
-            <div className="col-xs-6">
+            <div className="col-xs-12 col-md-6">
               <p className="left">版权所有©2018 FCC成都社区保留</p>
             </div>
-            <div className="col-xs-6">
+            <div className="col-xs-12 col-md-6">
               <p className="right">
                 FCC成都社区
               </p>
