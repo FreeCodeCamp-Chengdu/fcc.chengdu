0d9884baea1cd7da17015dc0f035726bf3ab7048
South Drifted <shiy2008@gmail.com>
2024-09-08 21:38:40 +0000

    [add] Code Formatter & Git hooks [migrate] replace hand-written Service Worker with Workbox [migrate] replace Yarn with PNPM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './layouts/Index';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { serviceWorkerUpdate } from 'web-utility';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './layouts/Index';
+
+const { serviceWorker } = window.navigator,
+    NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (NODE_ENV !== 'development')
+    serviceWorker
+        ?.register('sw.js')
+        .then(serviceWorkerUpdate)
+        .then(worker => {
+            if (confirm('New version of this Web App detected, update now?'))
+                worker.postMessage({ type: 'SKIP_WAITING' });
+        });
+
+serviceWorker?.addEventListener('controllerchange', () => location.reload());
+
+render(<App />, document.getElementById('root'));
