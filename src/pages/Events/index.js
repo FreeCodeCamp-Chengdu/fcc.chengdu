9e80f89110e8e43d90ece15009ef814aaa16a4b6
South Drifted <shiy2008@gmail.com>
2024-09-08 21:38:40 +0000

    [add] Code Formatter & Git hooks [migrate] replace hand-written Service Worker with Workbox [migrate] replace Yarn with PNPM

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,7 +1,7 @@
 /**
  * @desc 历史活动页面
- * @author 
-*/
+ * @author
+ */
 import './style.scss';
 
 import React, { Component } from 'react';
@@ -12,8 +12,8 @@ class Events extends Component {
     constructor() {
         super();
         let pos = {
-            '1': 'left',
-            '-1': 'right',
+            1: 'left',
+            '-1': 'right'
         };
         let list = [];
         let initPos = -1;
@@ -21,7 +21,8 @@ class Events extends Component {
             let s = eventList[k].time.split('');
             initPos *= -1;
             eventList[k].pos = pos[`${initPos}`];
-            eventList[k].date = `${s[2]}${s[3]}年${s[4] || ''}${s[5]}月${s[6] || ''}${s[7]}日`;
+            eventList[k].date =
+                `${s[2]}${s[3]}年${s[4] || ''}${s[5]}月${s[6] || ''}${s[7]}日`;
             list.unshift(eventList[k]);
         }
         this.state = {
@@ -29,47 +30,55 @@ class Events extends Component {
         };
     }
 
-    componentDidMount(){
-        window.$(document).ready(function () {
-            window.$('.VivaTimeline').vivaTimeline({
-                carousel: true,
-                carouselTime: 3000
-            });
-        })
+    componentDidMount() {
+        window.$('.VivaTimeline').vivaTimeline({
+            carousel: true,
+            carouselTime: 3000
+        });
     }
 
     render() {
         return (
             <div className="container">
-                <link rel="stylesheet" type="text/css" href="css/jquery.eeyellow.Timeline.css" />
+                <link
+                    rel="stylesheet"
+                    href="css/jquery.eeyellow.Timeline.css"
+                />
                 <div className="row">
                     <div className="col-md-12">
                         <div className="VivaTimeline">
                             <dl>
-                                {
-                                    this.state.list.map((item, index) => {
-                                        return <div key={`event-panel-${index}`} >
-
-                                            <dd className={`pos-${item.pos} clearfix`}>
-                                                <div className="circ"></div>
-                                                <div className="time">{item.date}</div>
-                                                <div className="events">
-                                                    <div className="events-header">{item.type} - {item.title}</div>
-                                                    <div className="events-body">
-                                                        <div className="">
-                                                            <div className="col-md-6 pull-left">
-                                                                <img alt={`${item.title}`} className="events-object img-responsive img-rounded" src={`${item.imageList[0].url}`} />
-                                                            </div>
-                                                            <div className="events-desc">
-                                                                {item.describe}
-                                                            </div>
+                                {this.state.list.map((item, index) => (
+                                    <div key={`event-panel-${index}`}>
+                                        <dd
+                                            className={`pos-${item.pos} clearfix`}
+                                        >
+                                            <div className="circ"></div>
+                                            <div className="time">
+                                                {item.date}
+                                            </div>
+                                            <div className="events">
+                                                <div className="events-header">
+                                                    {item.type} - {item.title}
+                                                </div>
+                                                <div className="events-body">
+                                                    <div className="">
+                                                        <div className="col-md-6 pull-left">
+                                                            <img
+                                                                alt={`${item.title}`}
+                                                                className="events-object img-responsive img-rounded"
+                                                                src={`${item.imageList[0].url}`}
+                                                            />
+                                                        </div>
+                                                        <div className="events-desc">
+                                                            {item.describe}
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </dd>
-                                        </div>
-                                    })
-                                }
+                                            </div>
+                                        </dd>
+                                    </div>
+                                ))}
                             </dl>
                         </div>
                     </div>
