82402dd91c888183483d740a7c41ca9bb39b85cd
luojiyin <luojiyin@hotmail.com>
2024-10-15 22:41:29 +0800

    refactor: use Bootstrap css class

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,26 +3,30 @@ import FLogo from '../assets/images/fcc-logo.png';
 
 export const Footer = () => (
     <footer className="Footer">
-        <div className="row justify-content-center align-items-center footer-bg">
-            <div className="col-xs-12 col-md-6">
-                <div className="left-content">
-                    <img src={FLogo} alt="FCC logo"></img>
-                    <p>"人人皆可编程"营造有温度的技术社区</p>
+        <div className="container-fluid footer-bg">
+            <div className="row align-items-center">
+                <div className="col-md-6">
+                    <div className="left-content">
+                        <img src={FLogo} alt="FCC logo" className="img-fluid" />
+                        <p>"人人皆可编程"营造有温度的技术社区</p>
+                    </div>
                 </div>
-            </div>
-            <div className="col-xs-12 col-md-6">
-                <div className="right-content">
-                    <About />
+                <div className="col-md-6">
+                    <div className="right-content">
+                        <About />
+                    </div>
                 </div>
             </div>
         </div>
-        <div className="info justify-content-center align-items-center">
+        <div className="container-fluid info">
             <div className="row">
-                <div className="col-xs-12 col-md-6">
-                    <p className="left">版权所有©2018 FCC成都社区保留</p>
+                <div className="col-md-6">
+                    <p className="text-md-start">
+                        版权所有©2018 FCC成都社区保留
+                    </p>
                 </div>
-                <div className="col-xs-12 col-md-6">
-                    <p className="right">FCC成都社区</p>
+                <div className="col-md-6">
+                    <p className="text-md-end">FCC成都社区</p>
                 </div>
             </div>
         </div>
